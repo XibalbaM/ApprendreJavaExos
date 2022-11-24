687bd17905db55a5f440456946518a8b18dfebce
Xibalba <xibalba2minecraft@gmail.com>
2022-11-24 18:04:00 +0100

    Commit

--- src/main/java/fr/xibalba/apprendreJavaExos/Main.java
+++ src/main/java/fr/xibalba/apprendreJavaExos/Main.java
@@ -1,34 +1,9 @@
 package fr.xibalba.apprendreJavaExos;
 
-import fr.xibalba.apprendreJavaExos.candidats.Candidat;
-import fr.xibalba.apprendreJavaExos.candidats.Chocolatier;
-import fr.xibalba.apprendreJavaExos.candidats.Glacier;
-import fr.xibalba.apprendreJavaExos.candidats.Patissier;
-import fr.xibalba.apprendreJavaExos.desserts.Dessert;
-import fr.xibalba.apprendreJavaExos.desserts.Gateau;
-import fr.xibalba.apprendreJavaExos.desserts.Glace;
-import fr.xibalba.apprendreJavaExos.desserts.MontageChocolat;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        Candidat candidat = new Candidat("Ferrau Gauthier", 1);
-        Dessert paris_brest = candidat.realiserDessert("Paris Brest");
 
-        Patissier patissier = new Patissier("Voleta Labossière", 2);
-        Gateau foret_noire = patissier.realiserDessert("Forêt Noire");
-        foret_noire.cuireAuFour();
-
-        Chocolatier chocolatier = new Chocolatier("Annette Lessard", 3);
-        MontageChocolat oeuf_chocolat = chocolatier.realiserDessert("Oeuf de Pâque en chocolat");
-        oeuf_chocolat.assemblerPieceDeChocolat();
-
-        Glacier glacier = new Glacier("Emmanuel Picard", 4);
-        Glace glace = glacier.realiserDessert("Glace Vanille-Fraise revisitée");
-        glace.mettreAuCongelateur();
-
-        Concours concours = new Concours(paris_brest, glace, foret_noire, oeuf_chocolat);
-        concours.designerGagnant();
     }
 }
---
+++ src/main/java/fr/xibalba/apprendreJavaExos/objets/BoiteDeSardines.java
@@ -0,0 +1,4 @@
+package fr.xibalba.apprendreJavaExos.objets;
+
+public class BoiteDeSardines {
+}
