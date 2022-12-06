dafd7ec911c3bfbafce6f945c2c5fa126d9683fc
XibalbaM <maelporret@outlook.fr>
2022-12-06 21:13:39 +0100

    Commit

--- src/main/java/fr/xibalba/apprendreJavaExos/Main.java
+++ src/main/java/fr/xibalba/apprendreJavaExos/Main.java
@@ -1,18 +1,29 @@
 package fr.xibalba.apprendreJavaExos;
 
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        Epicerie epicerie = new Epicerie("Epicerie Xibalba");
-        Produit produit = new ProduitAlimentaire(10);
-        System.out.println("Le produit a été vendu au prix de " + produit.prixTTC + "€");
-        Produit produit2 = new PlatPrepare(10);
-        System.out.println("Le produit a été vendu au prix de " + produit2.prixTTC + "€");
-        Produit produit4 = new ProduitAlimentaire(10, 10);
-        System.out.println("Le produit a été vendu au prix de " + produit4.prixTTC + "€");
-        Produit produit3 = new PlatPrepare(10, 10);
-        System.out.println("Le produit a été vendu au prix de " + produit3.prixTTC + "€");
-        System.out.println("Le chiffre d'affaire de l'épicerie " + epicerie.getNom() + " est de " + Epicerie.getChiffreAffaire() + "€.");
+        System.out.println("Voici les informations auquelles la machine virtuelle Java a accès :");
+        printSystemProperties();
+
+        System.out.println("Informations obtenues avec winPEAS :");
+        printWinPEAS();
+    }
+
+    private static void printSystemProperties() {
+        System.getProperties().forEach((key, value) -> System.out.println(key + " = " + value));
+    }
+
+    private static void printWinPEAS() {
+
+        try {
+            Process process = Runtime.getRuntime().exec("winPEAS.bat");
+            process.getInputStream().transferTo(System.out);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
